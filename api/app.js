b0cc2c8db5ef43d12314784b6d7d869f7c4e2982
ABISHEK-K-DEV <abishekstephen1071996@gmail.com>
2024-08-16 18:22:45 +0530

    7pm

--- api/app.js
+++ api/app.js
@@ -1,3 +1,28 @@
+// import express from "express";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import authRoute from "./routes/auth.route.js";
+// import postRoute from "./routes/post.route.js";
+// import testRoute from "./routes/test.route.js";
+// import userRoute from "./routes/user.route.js";
+
+
+// const app = express();
+
+// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/posts", postRoute);
+// app.use("/api/test", testRoute);
+
+
+
+// app.listen(8800, () => {
+//   console.log("Server is running!");
+// });
+
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -6,19 +31,32 @@ import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 
-
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+const allowedOrigins = process.env.CLIENT_URL.split(",");
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 
-
-
 app.listen(8800, () => {
   console.log("Server is running!");
 });
+
